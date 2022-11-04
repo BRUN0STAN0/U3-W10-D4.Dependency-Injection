2a2d6cd8685cf9c1f06e4b15fd6624ae34b7731d
Bruno <brunostano00@protonmail.com>
2022-11-04 09:37:24 +0100

    initial

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss'],
+})
+export class TestComponent implements OnInit {
+  @Input() titolo?: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
